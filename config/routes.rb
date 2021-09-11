e43b8470894ca12291e453c51e78d6a36b37950f
George Dilthey <george.dilthey@gmail.com>
2021-09-11 13:08:38 -0400

    refactored login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   resources :scores
   resources :users
-
-  get "/login", to: "sessions#new", as: "login"
-  post "/login", to: "sessions#create"
-  delete "/login", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
