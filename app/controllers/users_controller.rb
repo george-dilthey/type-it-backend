55189ecab07a36d5f001f7ec9500c506663ec326
George Dilthey <george.dilthey@gmail.com>
2021-09-10 20:51:46 -0400

    added index action for scores

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,8 @@
 class UsersController < ApplicationController
 
-    def new
-        user = User.new
-    end
-
-    def create
-        user = User.new(user_params)
-        if user.save
-            [session[:user_id] = @user.id]
-            render json: user
-        else
-            #error
-        end
-    end
-
     def index
         users = User.all
         render json: users
     end
 
-    private
-
-    def user_params
-        params.require(:user.permit(:username))
-    end
 end
